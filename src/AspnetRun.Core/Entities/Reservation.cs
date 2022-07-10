927bbb688585d6130b7678b7e706674e2107d506
DESKTOP-TUE9SV7\user <mohammed.albitar@makane.com>
2022-07-10 13:41:58 +0300

    updateReservationsTable

--- src/AspnetRun.Core/Entities/Reservation.cs
+++ src/AspnetRun.Core/Entities/Reservation.cs
@@ -7,12 +7,14 @@ using System.Threading.Tasks;
 
 namespace AspnetRun.Core.Entities
 {
-    public class Reservation:Entity
+    public class Reservation : Entity
     {
         public DateTime From { get; set; }
         public DateTime To { get; set; }
         public int CourtId { get; set; }
         public Court Court { get; set; }
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
 
     }
 }
---
+++ src/AspnetRun.Infrastructure/Migrations/20220710104133_updateReservationsTable.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AspnetRun.Infrastructure.Migrations
+{
+    public partial class updateReservationsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "Reservations",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Reservations",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Reservations");
+        }
+    }
+}
